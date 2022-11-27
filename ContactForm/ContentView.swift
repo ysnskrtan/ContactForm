8c372a1c113698b78559fab0cf3d1572ca69914f
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-11-27 14:02:53 +0300

    Text fields, text editor, picker, button and sections added to contact form

--- ContactForm/ContentView.swift
+++ ContactForm/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var emailAddress = ""
+    @State private var phoneNumber = ""
+    @State private var message = "Write your message here..."
+    @State private var subject = "Help"
+    @State private var includeLogs = false
+    
+    @State private var preferredContactMethod = ContactMethod.email
+    
+    enum ContactMethod {
+        case email, phone
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section(header: Text("How can we reach you?")) {
+                    Picker("Contact Method", selection: $preferredContactMethod) {
+                        Text("Email").tag(ContactMethod.email)
+                        Text("Phone").tag(ContactMethod.phone)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    switch preferredContactMethod {
+                    case .email:
+                        TextField("Email Address", text: $emailAddress)
+                            .textContentType(.emailAddress)
+                            .disableAutocorrection(true)
+                    case .phone:
+                        TextField("Phone Number", text: $phoneNumber)
+                    }
+                }
+                Section(header: Text("What can we help you with?")) {
+                    Picker("Subject", selection: $subject) {
+                        ForEach(["Help", "Suggestion", "Bug Report"], id: \.self) { subject in
+                            Text(subject)
+                        }
+                    }
+                }
+                Section(header: Text("Briefly explain what‘s going on.")) {
+                    TextEditor(text: $message)
+                }
+                Section(footer: Text("This information will be sent anonymously.")) {
+                    Toggle("Include Logs", isOn: $includeLogs)
+                }
+                Button("Submit", action: {
+                    print("Submit button tapped")
+                })
+            }
+            .navigationTitle("Contact Us")
         }
-        .padding()
     }
 }
 
