e1ecc945a61ccc814b98d8779e9f2c93fb6375e0
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-11-27 13:49:06 +0300

    Initial Commit

---
+++ ContactForm/ContactFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactFormApp.swift
+//  ContactForm
+//
+//  Created by Yasin Şükrü Tan on 27.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
